a481a160b46d670ab0c3ba614aae095fc6639086
haixi1818 <haixiz2018@gmail.com>
2025-07-10 20:42:18 -0500

    SCRUM-126 Add dto profile

---
+++ src/main/java/org/beaconfire/model/Employee.java
@@ -0,0 +1,60 @@
+package org.beaconfire.model;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Document(collection = "Employee")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Employee {
+
+    @Id
+    private String id;
+
+    private String userId;
+
+    // Basic Info
+    private String firstName;
+    private String lastName;
+    private String middleName;
+    private String preferredName;
+    private String email;
+    private String cellPhone;
+    private String alternatePhone;
+    private String gender;
+    private String ssn;
+    private LocalDate dob;
+
+    // Employment Info
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    private String workAuthType;
+    private LocalDate workAuthStartDate;
+    private LocalDate workAuthEndDate;
+
+    private String driverLicense;
+    private LocalDate driverLicenseExpiration;
+
+    // Housing Info
+    private String houseId;
+
+    // Embedded documents
+    private List<Address> addressList;
+    private List<Contact> contacts;
+    private List<VisaStatus> visaStatuses;
+    private List<PersonalDocument> personalDocuments;
+
+    // Status
+    private Boolean activeFlag;
+    private LocalDateTime createDate;
+    private LocalDateTime lastModificationDate;
+
+}
