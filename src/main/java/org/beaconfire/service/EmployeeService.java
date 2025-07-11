6ff2c72969bc28e88594fd5725f750cf345c72bb
haixi1818 <haixiz2018@gmail.com>
2025-07-11 11:11:38 -0500

    fix SCRUM-128 add test

--- src/main/java/org/beaconfire/service/EmployeeService.java
+++ src/main/java/org/beaconfire/service/EmployeeService.java
@@ -1,12 +1,14 @@
 package org.beaconfire.service;
 
 import org.beaconfire.dto.CreateEmployeeRequest;
+import org.beaconfire.dto.UpdateEmployeeRequest;
 import org.beaconfire.model.Employee;
 
 public interface EmployeeService {
 
     Employee getEmployeeById(String id);
     Employee registerEmployee(CreateEmployeeRequest request);
+    Employee updateEmployee(String id, UpdateEmployeeRequest request);
 
 
 }
