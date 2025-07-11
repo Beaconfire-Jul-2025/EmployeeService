63e981e80c37cb6c6944419a46692e0a79c175fe
haixi1818 <haixiz2018@gmail.com>
2025-07-11 10:56:52 -0500

    fix SCRUM-124 and merge with main

--- src/main/java/org/beaconfire/repository/EmployeeRepository.java
+++ src/main/java/org/beaconfire/repository/EmployeeRepository.java
@@ -4,7 +4,6 @@ import org.beaconfire.model.Employee;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 
 @Repository
 public interface EmployeeRepository extends MongoRepository<Employee, String> {
